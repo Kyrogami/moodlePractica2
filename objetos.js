546debcb9be64341409d11137496a1da9706ed6e
Kyrogami <124576995+Kyrogami@users.noreply.github.com>
2023-05-04 10:25:07 +0200

    array y bucle de datos funciona

--- objetos.js
+++ objetos.js
@@ -14,9 +14,37 @@ class usuario {
     }
   }
 
-//creo un alumno de usuario 
+//creo unos objetos usuarios de alumnos 
 let alumnoAdrian = new usuario(1, "Adrián", "Sánchez", "adri", "1234", "foto1.jpg", "Calle Alicante, Nº1", "677222555", "alumno", true);
-console.log()
+let alumnoJose = new usuario(2, "Jose", "González", "JoseGonz", "1234", "foto2.jpg", "Calle Cavanilles 456", "555-5678", "alumno", true);
+let alumnoPedro = new usuario(3, "Pedro", "Rodríguez", "peporro", "1234", "foto3.jpg", "Calle Espeñetas 789", "555-9012", "alumno", true);
+
+//creo mi array 
+let usuariosArray =[];
+
+//introducimos al array
+usuariosArray.push(alumnoAdrian);
+usuariosArray.push(alumnoJose);
+usuariosArray.push(alumnoPedro);
+
+//voy a crear un bucle para un array
+//creo un bucle; creo mi variable contador = 0; establezco condición 
+for (let contador = 0; contador < usuariosArray.length;  contador++){
+console.log("ID: " + usuariosArray[contador].id);
+console.log("Nombre: " + usuariosArray[contador].nombre);
+console.log("Apellidos: " + usuariosArray[contador].apellidos);
+console.log("Usuario: " + usuariosArray[contador].usuario);
+console.log("Contraseña: " + usuariosArray[contador].contraseña);
+console.log("Foto: " + usuariosArray[contador].foto);
+console.log("Dirección: " + usuariosArray[contador].direccion);
+console.log("Teléfono: " + usuariosArray[contador].telefono);
+console.log("Tipo de usuario: " + usuariosArray[contador].tipo_usuario);
+console.log("Activo: " + usuariosArray[contador].activo);
+}
+
+
+console.log(usuariosArray);
+
 
 //Vamos a crear infoAlumno pasandole (alumno) como una variable para renombrar
 class infoalumno {
@@ -75,3 +103,4 @@ console.log(informacionAlumnoAdrian.activo);
       this.id_profesor2 = id_profesor2;
     }
   }
+
