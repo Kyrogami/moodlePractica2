5bfd072552866092ad95519112552c8d16086293
Kyrogami <124576995+Kyrogami@users.noreply.github.com>
2023-05-05 12:53:26 +0200

    tengo el constructor en typescript pero no va xd

--- js/objetos.js
+++ js/objetos.js
@@ -116,8 +116,56 @@ const usuariosArray = [
   }
 ];
 
-Este código debería funcionar correctamente.
+//AHORA VAMOS A HACER CONSTRUCTOR//
 
+interface Usuario  {
+  name: string;
+  apellidos: string;
+  usuario: string;
+  contraseña: string;
+  foto: string;
+  direccion: string;
+  telefono: number[];
+  tipo_usuario: string;
+  activo: boolean;
+}
+
+let alumnoAdrian: Usuario = {
+  name: "Adrián",
+  apellidos: "Sánchez Martínez",
+  usuario: "adri",
+  contraseña: "1234",
+  foto: "foto1.png",
+  direccion: "Calle Alicante, Nº1",
+  telefono: [677225159],
+  tipo_usuario: "alumnos",
+  activo: true
+};
+
+let alumnoJose: Usuario = {
+  name: "Jose",
+  apellidos: "González García",
+  usuario: "JoseGonz",
+  contraseña: "1234",
+  foto: "foto2.png",
+  direccion: "Calle Cavanilles 456",
+  telefono: [5555678],
+  tipo_usuario: "alumnos",
+  activo: true
+};
+
+let alumnoJuan: Usuario = {
+  name: "Juan",
+  apellidos: "Pérez Fernández",
+  usuario: "juanpf",
+  contraseña: "1234",
+  foto: "foto3.png",
+  direccion: "Calle Espeñetas 789",
+  telefono: [5559012],
+  tipo_usuario: "alumnos",
+  activo: true
+}
+};
 
 
 
--- pruebas.js
+++ pruebas.js
@@ -12,3 +12,54 @@ for (let contador = 0; contador < usuariosArray.length;  contador++){
     console.log("Tipo de usuario: " + usuariosArray[contador].tipo_usuario);
     console.log("Activo: " + usuariosArray[contador].activo);
     }
+// intento para el array en typescript
+let alumno = new Object();
+
+interface alumno  {
+    name: string;
+    apellidos: string;
+    usuario: string;
+    contraseña: string;
+    foto: string;
+    direccion: string;
+    telefono: number[];
+    tipo_usuario: string;
+    activo: boolean;
+  }
+  
+  let alumnoAdrian: alumno = {
+    name: "Adrián",
+    apellidos: "Sánchez Martínez",
+    usuario: "adri",
+    contraseña: "1234",
+    foto: "foto1.png",
+    direccion: "Calle Alicante, Nº1",
+    telefono: [677225159],
+    tipo_usuario: "alumnos",
+    activo: true
+  }
+  
+  let alumnoJose: alumno = {
+    name: "Jose",
+    apellidos: "González García",
+    usuario: "JoseGonz",
+    contraseña: "1234",
+    foto: "foto2.png",
+    direccion: "Calle Cavanilles 456",
+    telefono: [5555678],
+    tipo_usuario: "alumnos",
+    activo: true
+  }
+  
+  let alumnoJuan: alumno = {
+    name: "Juan",
+    apellidos: "Pérez Fernández",
+    usuario: "juanpf",
+    contraseña: "1234",
+    foto: "foto3.png",
+    direccion: "Calle Espeñetas 789",
+    telefono: [5559012],
+    tipo_usuario: "alumnos",
+    activo: true
+  };
+  
