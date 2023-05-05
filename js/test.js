ce28c1ef00cb1b5e92eb70e8b6de76254fb6532e
Kyrogami <124576995+Kyrogami@users.noreply.github.com>
2023-05-05 14:04:00 +0200

    typescript funciona todo ook

---
+++ js/test.js
@@ -0,0 +1,35 @@
+// intento para el array en typescript
+var alumno = new Object();
+var alumnoAdrian = {
+    name: "Adrián",
+    apellidos: "Sánchez Martínez",
+    usuario: "adri",
+    contraseña: "1234",
+    foto: "foto1.png",
+    direccion: "Calle Alicante, Nº1",
+    telefono: [677225159],
+    tipo_usuario: "alumnos",
+    activo: true
+};
+var alumnoJose = {
+    name: "Jose",
+    apellidos: "González García",
+    usuario: "JoseGonz",
+    contraseña: "1234",
+    foto: "foto2.png",
+    direccion: "Calle Cavanilles 456",
+    telefono: [5555678],
+    tipo_usuario: "alumnos",
+    activo: true
+};
+var alumnoJuan = {
+    name: "Juan",
+    apellidos: "Pérez Fernández",
+    usuario: "juanpf",
+    contraseña: "1234",
+    foto: "foto3.png",
+    direccion: "Calle Espeñetas 789",
+    telefono: [5559012],
+    tipo_usuario: "alumnos",
+    activo: true
+};
--- js/test.ts
+++ js/test.ts
@@ -1,17 +1,3 @@
-//voy a crear un bucle para un array
-//creo un bucle; creo mi variable contador = 0; establezco condición 
-for (let contador = 0; contador < usuariosArray.length;  contador++){
-    console.log("ID: " + usuariosArray[contador].id);
-    console.log("Nombre: " + usuariosArray[contador].nombre);
-    console.log("Apellidos: " + usuariosArray[contador].apellidos);
-    console.log("Usuario: " + usuariosArray[contador].usuario);
-    console.log("Contraseña: " + usuariosArray[contador].contraseña);
-    console.log("Foto: " + usuariosArray[contador].foto);
-    console.log("Dirección: " + usuariosArray[contador].direccion);
-    console.log("Teléfono: " + usuariosArray[contador].telefono);
-    console.log("Tipo de usuario: " + usuariosArray[contador].tipo_usuario);
-    console.log("Activo: " + usuariosArray[contador].activo);
-    }
 // intento para el array en typescript
 let alumno = new Object();
 
