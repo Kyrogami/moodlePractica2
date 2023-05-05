9bcd5998243ac94aa9ae7ab7c36b2f3e19951c64
Kyrogami <124576995+Kyrogami@users.noreply.github.com>
2023-05-05 11:54:35 +0200

    primer prototipo de array en typesscript funciona

--- js/objetos.js
+++ js/objetos.js
@@ -44,11 +44,102 @@ for (let contador = 0; contador < usuariosArray.length; contador++) {
   console.log(usuariosArray[contador]);
 }
 
+/* Vamos a powershell 
+
+Hay que tener instalado node primero
+1. -> npm install -g typescript // npm install --save-dev typescript //Instalamos
+2. -> npm init //iniciamos.
+3. -> tsc -v // comprobar version.
+
+Ya  tenemos listo typeScript
+
+
+EJEMPLO ARRAY
+{
+"arrayname":[{
+"nombrecolor":"rojo",
+"+datos":"datos",
+"+datos":"datos"
+"
+},
+"nombrecolor":"rojo",
+"+datos":"datos",
+"+datos":"datos"
+"},
+"nombrecolor":"rojo",
+"+datos":"datos",
+"+datos":"datos"
+"}
+}
+}
+
+// PRIMER INTENTO Y PLANTILLA PARA DATOS //
+{
+"usuariosArray":[
+  {
+    "nombre":"",
+    "apellidos":"",
+    "usuario":"",
+    "contraseña":"",
+    "foto":"",
+    "direccion":"",
+    "telefono":"",
+    "tipo_usuario":"",
+    "activo":""
+]
+  }
+}
+
+//////// DATOS PARA TYPESCRIPT FUNCIONA ///////////
+const usuariosArray = [
+  {
+    "nombre":"Adrian",
+    "apellidos":"Sánchez",
+    "usuario": "adri",
+    "contraseña": 1234,
+    "foto": "foto1.jpg",
+    "direccion": "Calle Alicante, Nº1",
+    "telefono": 677222555,
+    "tipo_usuario": "alumno",
+    "activo": true
+  },
+  {
+    "nombre": "Jose",
+    "apellidos": "Sanchez",
+    "usuario": "schema",
+    "contraseña": 1234,
+    "foto": "foto2.jpg",
+    "direccion": "Calle Cavanilles, Nº1",
+    "telefono": 617252555,
+    "tipo_usuario": "alumno",
+    "activo": true
+  }
+];
+
+Este código debería funcionar correctamente.
 
 
 
 
 
+
+
+*/
+
+
+
+"Adrián", "Sánchez", "adri", "1234", "foto1.jpg", "Calle Alicante, Nº1", "677222555", "alumno", true
+
+this.id = id;
+this.nombre = nombre;
+this.apellidos = apellidos;
+this.usuario = usuario;
+this.contraseña = contraseña;
+this.foto = foto;
+this.direccion = direccion;
+this.telefono = telefono;
+this.tipo_usuario = tipo_usuario;
+this.activo = activo;
 
 
 
